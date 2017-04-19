f095717778c776b20ba561306b45cb0847a08272
MattZelda <mattheawesome1@gmail.com>
2017-04-19 12:54:47 -0400

    Add project files.

---
+++ PassGen/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PassGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.MinLength = new System.Windows.Forms.TextBox();
+            this.MaxLength = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Uppercase";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 65);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(78, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Lowercase";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 88);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(61, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Special";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 111);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(68, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Numbers";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Min length: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(172, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Max length: ";
+            // 
+            // MinLength
+            // 
+            this.MinLength.Location = new System.Drawing.Point(258, 40);
+            this.MinLength.Name = "MinLength";
+            this.MinLength.Size = new System.Drawing.Size(71, 20);
+            this.MinLength.TabIndex = 10;
+            this.MinLength.TextChanged += new System.EventHandler(this.MinLength_TextChanged);
+            // 
+            // MaxLength
+            // 
+            this.MaxLength.Location = new System.Drawing.Point(258, 75);
+            this.MaxLength.Name = "MaxLength";
+            this.MaxLength.Size = new System.Drawing.Size(71, 20);
+            this.MaxLength.TabIndex = 11;
+            this.MaxLength.TextChanged += new System.EventHandler(this.MaxLength_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 261);
+            this.Controls.Add(this.MaxLength);
+            this.Controls.Add(this.MinLength);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "PassGen v2.1.4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox MinLength;
+        private System.Windows.Forms.TextBox MaxLength;
+    }
+}
+
---
+++ PassGen/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                Upper = true;
+            }
+            else
+            {
+                Upper = false;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                Lower = true;
+            }
+
+            else
+            {
+                Lower = false;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                Special = true;
+            }
+
+            else
+            {
+                Special = false;
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                Numbers = true;
+            }
+
+            else
+            {
+                Numbers = false;
+            }
+        }
+
+        //Here's what I want to do...
+        //If the minimum is less than 1, delete the text in textbox, and have them re-enter the value until it is valid.
+        private void MinLength_TextChanged(object sender, EventArgs e)
+        {
+            int min;
+            if (int.TryParse(MinLength.Text, out min))
+            {
+                if (min >= 1)
+                    Min = min;
+            }
+        }
+
+        private void MaxLength_TextChanged(object sender, EventArgs e)
+        {
+            int max;
+            if (int.TryParse(MaxLength.Text, out max))
+            {
+                if (max >= 1)
+                {
+                    Max = max;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Min < 1) { MessageBox.Show("Invalid minimum range."); }
+            if(Max < Min) { MessageBox.Show("Invalid maximum range."); }
+            textBox1.Text = GeneratePass(Min, Max, Upper, Lower, Special, Numbers);
+        }
+
+        public static int randNum(int min, int max)
+        {
+            //Locks the random method to a single thread.
+            lock(syncLock)
+            {
+                return random.Next(min, max);
+            }
+        }
+
+        public static string GeneratePass(int min, int max, bool upper, bool lower, bool special, bool numbers)
+        {
+
+            string init = " ";
+
+            if(upper)
+            {
+                init += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            }
+
+            if(lower)
+            {
+                init += "abcdefghijklmnopqrstuvwxyz";
+            }
+
+            if(special)
+            {
+                init += " {}!@#$%^&*()_-\\\'\"+=,./;:<=>?[]{}|`~";
+            }
+
+            if(numbers)
+            {
+                init += "0123456789";
+            }
+
+            char[] generate = init.ToCharArray();
+
+            int rand = randNum(min, max);
+
+            byte[] data = new byte[1];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetNonZeroBytes(data);
+                data = new byte[rand];
+                crypto.GetNonZeroBytes(data);
+            }
+
+            StringBuilder result = new StringBuilder(rand);
+            foreach(byte b in data)
+            {
+                result.Append(generate[b % (generate.Length)]);
+            }
+
+            return result.ToString();
+        }
+
+        //Data members.
+        public int Min { get; set; } //Minimum length of string
+        public int Max { get; set; } //Maximum length of string
+        public bool Upper { get; set; } //Uppercase?
+        public bool Lower { get; set; } //Lowercase?
+        public bool Special { get; set; } //Special?
+        public bool Numbers { get; set; } //Numbers allowed?
+
+        //Random number generator sequence.
+        private static readonly Random random = new Random(); //The readonly cast means that it cannot be set, only get.
+        private static readonly object syncLock = new object();
+
+    }
+}
